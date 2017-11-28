6b7c4179b3cc9b6ef580f739f248dd0c1b4a0d5e
koozy <koozhengyu@gmail.com>
2017-11-28 10:44:00 +0800

    updating delivery_order factory

--- spec/factories/delivery_orders.rb
+++ spec/factories/delivery_orders.rb
@@ -2,7 +2,6 @@ require 'faker'
 
 FactoryBot.define do
   factory :delivery_order do
-    order_id "GO00001"
     serving_datetime {Faker::Time.between(DateTime.now - 1, DateTime.now).beginning_of_hour()}
   end
 end
